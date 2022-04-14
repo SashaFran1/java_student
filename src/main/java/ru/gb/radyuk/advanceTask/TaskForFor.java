5ab8927116ca3f4605b83a81840ccde0e6a89979
Aleksandr Radyuk <Sashaqweert@gmail.com>
2022-04-14 12:22:06 +0500

    Complete oneSquareAndLine

--- src/main/java/ru/gb/radyuk/advanceTask/TaskForFor.java
+++ src/main/java/ru/gb/radyuk/advanceTask/TaskForFor.java
@@ -18,7 +18,25 @@ public class TaskForFor {
         }
     }
 
+    private static void oneSquareAndLine() {
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i == 0 || i == 5) {
+                    System.out.print("*  ");
+                }
+                if (i != 0 && i != 5) {
+                    if (j == 0 || j == 5 || i == j) System.out.print("*  ");
+                    else System.out.print("   ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
         oneSquare();
+        System.out.println();
+        oneSquareAndLine();
     }
 }
