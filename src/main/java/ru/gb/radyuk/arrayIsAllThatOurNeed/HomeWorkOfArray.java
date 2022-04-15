f661a16e0bccdeadbf15fbca4bb170a56270b995
SashaFran <Sashaqweert@gmail.com>
2022-04-16 00:14:12 +0500

    Complete third task in the home work

--- src/main/java/ru/gb/radyuk/arrayIsAllThatOurNeed/HomeWorkOfArray.java
+++ src/main/java/ru/gb/radyuk/arrayIsAllThatOurNeed/HomeWorkOfArray.java
@@ -25,8 +25,19 @@ public class HomeWorkOfArray {
 
     private static void summonerOfValueForArray() {
         int[] array = new int[100];
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < array.length; i++) {
             array[i] = i + 1;
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void multiplyToTwo() {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *=2;
+            }
             System.out.print(array[i] + " ");
         }
     }
@@ -35,5 +46,7 @@ public class HomeWorkOfArray {
         converterOfOneAndZero();
         System.out.println();
         summonerOfValueForArray();
+        System.out.println();
+        multiplyToTwo();
     }
 }
