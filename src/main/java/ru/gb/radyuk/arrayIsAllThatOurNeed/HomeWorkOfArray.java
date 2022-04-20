674d0c68f4d7f31ad3fdc1d9ce399d45efa054c7
SashaFran <Sashaqweert@gmail.com>
2022-04-20 15:49:20 +0500

    Complete fifth and sixth tasks in the home work

--- src/main/java/ru/gb/radyuk/arrayIsAllThatOurNeed/HomeWorkOfArray.java
+++ src/main/java/ru/gb/radyuk/arrayIsAllThatOurNeed/HomeWorkOfArray.java
@@ -36,7 +36,7 @@ public class HomeWorkOfArray {
         int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < array.length; i++) {
             if (array[i] < 6) {
-                array[i] *=2;
+                array[i] *= 2;
             }
             System.out.print(array[i] + " ");
         }
@@ -44,14 +44,15 @@ public class HomeWorkOfArray {
     }
 
     public static int[][] createMatrix(int size) {
-        int[][] a = new int [size][size];
-        for(int i=0; i<size; i++) {
+        int[][] a = new int[size][size];
+        for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 a[i][j] = (i == j) || (i + j == size - 1) ? 1 : 0;
             }
         }
         return a;
     }
+
     public static void printMatrix(int[][] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
@@ -61,6 +62,35 @@ public class HomeWorkOfArray {
         }
     }
 
+    private static int[] makeArrayOfValue(int len, int initialValue) {
+        int[] arrayOfValue = new int[len];
+        for (int i = 0; i < len; i++) {
+            arrayOfValue[i] = initialValue;
+        }
+        return arrayOfValue;
+    }
+
+    private static void printArrayOfValue(int[] a) {
+        for (int value : a) {
+            System.out.print(value + " ");
+        }
+    }
+
+    private static void minAndMaxOfArray() {
+        int[] randomValue = new int[20];
+        Random random = new Random();
+        int min = 50, max = 0;
+        for (int i = 0; i < randomValue.length; i++) {
+            randomValue[i] = random.nextInt(50);;
+            if (randomValue[i] < min) {
+                min = randomValue[i];
+            } else if (randomValue[i] > max) {
+                max = randomValue[i];
+            }
+        }
+        System.out.println("Минимальный элемент = " + min);
+        System.out.println("Максимальный элемент = " + max);
+    }
 
     public static void main(String[] args) {
         converterOfOneAndZero();
@@ -71,5 +101,9 @@ public class HomeWorkOfArray {
         System.out.println();
         int[][] a = createMatrix(9);
         printMatrix(a);
+        System.out.println();
+        printArrayOfValue(makeArrayOfValue(10, 15));
+        System.out.println();
+        minAndMaxOfArray();
     }
 }
