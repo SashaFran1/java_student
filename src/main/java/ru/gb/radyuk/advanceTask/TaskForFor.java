385b26f1427f6356e45881e55c8b16e81bd66a18
SashaFran <Sashaqweert@gmail.com>
2022-05-04 17:33:08 +0500

    Create random Print number With Weight

--- src/main/java/ru/gb/radyuk/advanceTask/TaskForFor.java
+++ src/main/java/ru/gb/radyuk/advanceTask/TaskForFor.java
@@ -1,5 +1,8 @@
 package ru.gb.radyuk.advanceTask;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class TaskForFor {
 
     private static void oneSquare() {
@@ -75,15 +78,64 @@ public class TaskForFor {
         }
     }
 
+    private static void sort(int[] a){
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j+1]) {
+                    int trash = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = trash;
+                }
+            }
+        }
+        for (int number: a ) {
+            System.out.print(number + "  ");
+        }
+    }
+
+    private static int[] createArray() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("Введите длину массива");
+        int[] a = new int[scanner.nextInt()];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = random.nextInt(8);
+        }
+        return a;
+    }
+
+    private static void randomPrintWithWeight(int[] arrayNumbers, int[] arrayWeight) {
+        int count = 0;
+        for (int i = 0; i < arrayNumbers.length; i++) {
+            count += arrayWeight[i];
+        }
+        int[] arrayWithWeight = new int[count];
+        int index = 0;
+        for (int i = 0; index < arrayWithWeight.length - 1; i++) {
+            int j = 0;
+            while (j < arrayWeight[i]) {
+                arrayWithWeight[index] = arrayNumbers[i];
+                j++;
+                index++;
+            }
+        }
+        int random = (int) (Math.random() * ( arrayWithWeight.length - 1));
+        System.out.println(arrayWithWeight[random]);
+    }
+
     public static void main(String[] args) {
-        oneSquare();
+//        oneSquare();
+//        System.out.println();
+//        oneSquareAndLine();
+//        System.out.println();
+//        triangle();
+//        System.out.println();
+//        SquareInSquare();
+//        System.out.println();
+//        twoSquares();
+//        System.out.println();
+//        sort(createArray());
         System.out.println();
-        oneSquareAndLine();
-        System.out.println();
-        triangle();
-        System.out.println();
-        SquareInSquare();
-        System.out.println();
-        twoSquares();
+        randomPrintWithWeight(createArray(),createArray());
     }
 }
