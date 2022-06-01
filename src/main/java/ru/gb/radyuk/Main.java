d20e4648a9b419180752e8e77a3a1471deea88c0
SashaFran <Sashaqweert@gmail.com>
2022-06-01 16:23:11 +0500

    Fix problems

--- src/main/java/ru/gb/radyuk/Main.java
+++ src/main/java/ru/gb/radyuk/Main.java
@@ -5,10 +5,10 @@ import java.util.Random;
 
 public class Main {
 
-    final static private int heightArray = 5;
+    final static private int heightArray = 4;
     final static private int weightArray = 4;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         counter(creatorArray());
     }
 
@@ -32,31 +32,32 @@ public class Main {
         return mainArr;
     }
 
-    public static void counter(String[][] arr1) {
-        try {
-            String[][] trashArr = new String[4][4];
-            int height = arr1.length, weight = arr1[0].length;
-            if (weight == 4 && height == 4) {
-                System.out.println("Длины совпадают");
-            } else throw new Exception("MyArraySizeException");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        String[] numbers = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",};
-        int count = 0;
-        for (int i = 0; i < heightArray; i++) {
-            for (int j = 0; j < weightArray; j++) {
-                try {
-                if ((Arrays.asList(numbers).contains(arr1[i][j]))) {
-                    count += Integer.parseInt(arr1[i][j]);
-                } else
-                    throw new Exception("MyArrayDataException");
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
+    public static void counter(String[][] arr1) throws Exception {
+        String[][] trashArr = new String[4][4];
+        int height = arr1.length, weight = arr1[0].length;
+        if (weight == 4 && height == 4) {
+            System.out.println();
+            System.out.println("Длины совпадают");
+            System.out.println();
+            String[] numbers = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",};
+            int count = 0;
+            for (int i = 0; i < heightArray; i++) {
+                for (int j = 0; j < weightArray; j++) {
+                    try {
+                        if ((Arrays.asList(numbers).contains(arr1[i][j]))) {
+                            count += Integer.parseInt(arr1[i][j]);
+                        } else
+                            throw new Exception("Ошибка данных в ячейке " + i + " - " + j);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
                 }
             }
+            System.out.println();
+            System.out.println("Сумма всех чисел массива = " + count);
+        } else {
+            throw new Exception("ОШИБКА!!! Несовпадение размеров массива");
         }
-        System.out.println(count);
     }
 }
 
