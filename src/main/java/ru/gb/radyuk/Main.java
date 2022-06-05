fa7d93a5762fd32dd1aa844271f2076409b5c909
SashaFran <Sashaqweert@gmail.com>
2022-06-05 20:48:36 +0500

    Done home work

--- src/main/java/ru/gb/radyuk/Main.java
+++ src/main/java/ru/gb/radyuk/Main.java
@@ -1,9 +1,17 @@
 package ru.gb.radyuk;
 
-import ru.gb.radyuk.unicWords.UnicWords;
+import ru.gb.radyuk.unicWords.*;
+import static ru.gb.radyuk.unicWords.PhoneBook.*;
 
 public class Main {
     public static void main(String[] args) {
         UnicWords.creatorArray();
+        addPhoneNumber( "89996546999", "Сидоров");
+        addPhoneNumber("89568741258", "Петров");
+        addPhoneNumber("89568743258", "Петров");
+        addPhoneNumber("89566284918", "Петров");
+        addPhoneNumber( "89567354918", "Сидоров");
+        addPhoneNumber("89112284918", "Иванов");
+        findNumber("Петров");
     }
 }
--- src/main/java/ru/gb/radyuk/unicWords/PhoneBook.java
+++ src/main/java/ru/gb/radyuk/unicWords/PhoneBook.java
@@ -1,7 +1,22 @@
 package ru.gb.radyuk.unicWords;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PhoneBook {
+
+    private static HashMap<String, String> phoneBook = new HashMap<>();
+
+    public static void addPhoneNumber(String number, String lastName) {
+        phoneBook.put(number, lastName);
+    }
+
+    public static void findNumber(String lastName) {
+        for (Map.Entry<String, String> pair : phoneBook.entrySet()) {
+            if (pair.getValue().equals(lastName)) {
+                System.out.println(lastName + " - " + pair.getKey());
+            }
+        }
+    }
 
 }
