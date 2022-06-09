a9c98fdb1bc5f3385ab07300a5ca8fe7c91672cb
SashaFran <Sashaqweert@gmail.com>
2022-06-09 17:26:32 +0500

    Done lesson 4

---
+++ src/main/java/ru/gb/radyuk/Frame.java
@@ -0,0 +1,23 @@
+package ru.gb.radyuk;
+
+import javax.swing.*;
+import java.awt.*;
+
+class Frame extends JFrame {
+
+    Frame() {
+        setTitle("Chat");
+        setSize(new Dimension(400, 400));
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        TextArea bigTextPanel = new TextArea();
+        SendPanel sendPanel = new SendPanel(bigTextPanel.getTextArea());
+
+        add(bigTextPanel, BorderLayout.CENTER);
+        add(sendPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+}
